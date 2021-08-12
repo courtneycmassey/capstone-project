a8e9e44ca15f596d57a9844d12ffcf6a530c6936
Courtney <courtneycmassey@gmail.com>
2021-08-12 09:58:11 -0700

    collects user information into DB upon signup

--- src/Signup.js
+++ src/Signup.js
@@ -4,25 +4,28 @@ import app, { usersCollection } from './utilities/firebase.js';
 
 const SignUp = ({ history }) => {
 
-    const storeUserInfo = (data, email, password) => {
+    const storeUserInfo = (data, email, password, firstName, lastName, userType) => {
 
         usersCollection
         .doc(data.user.uid)
         .set({
             email: email,
-            password: password
+            password: password,
+            first_name: firstName,
+            last_name: lastName,
+            user_type: userType
         })
     }
 
     const handleSignUp = useCallback(async event => {
         event.preventDefault();
-        const { email, password } = event.target.elements;
+        const { userType, firstName, lastName, email, password } = event.target.elements;
         try {
             await app
                 .auth()
                 .createUserWithEmailAndPassword(email.value, password.value)
                 .then( (data) => {
-                    storeUserInfo(data, email.value, password.value);
+                    storeUserInfo(data, email.value, password.value, firstName.value, lastName.value, userType.value);
                 }
                 );
             history.push("/");
@@ -36,6 +39,27 @@ const SignUp = ({ history }) => {
         <div>
             <h1>Sign up</h1>
             <form onSubmit={handleSignUp}>
+            <label>
+                User Type:
+                <input 
+                    name="userType" 
+                    type="text" 
+                    placeholder="student or teacher" />
+            </label>
+            <label>
+                First Name:
+                <input 
+                    name="firstName" 
+                    type="text" 
+                    placeholder="First Name" />
+            </label>
+            <label>
+                Last Name:
+                <input 
+                    name="lastName" 
+                    type="text" 
+                    placeholder="Last Name" />
+            </label>
             <label>
                 Email:
                 <input 
