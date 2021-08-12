8e5b5c74a71dacd7ec300f16c97e5111e359cb41
Courtney <courtneycmassey@gmail.com>
2021-08-12 15:18:09 -0700

    customizes nav bar for teacher and student; blocks students from teacher view and teachers from student view

--- src/components/header.js
+++ src/components/header.js
@@ -1,36 +1,93 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AuthContext } from '../Auth';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
     
-    //have conditional statements here for various nav bars based on login credential/status
+    const {userDetails} = useContext(AuthContext);
+
+    if (userDetails.user_type === 'student') {
+        return (
+            <header>
+                <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-info">
+                    <Link className="navbar-brand" to="/">Raise a Question</Link>
+                    <div className="collapse navbar-collapse" id="navbarCollapse">
+                        <ul className="navbar-nav mr-auto">
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/">Home</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/student_view">Student View</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </header>
+        );
+    } else if (userDetails.user_type === 'teacher') {
+        return (
+            <header>
+                <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-info">
+                    <Link className="navbar-brand" to="/">Raise a Question</Link>
+                    <div className="collapse navbar-collapse" id="navbarCollapse">
+                        <ul className="navbar-nav mr-auto">
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/">Home</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/class_form">Add Class</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/teacher_view">Teacher View</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </header>
+        );
+    } else {
+        return (
+            <header>
+                <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-info">
+                    <Link className="navbar-brand" to="/">Raise a Question</Link>
+                    <div className="collapse navbar-collapse" id="navbarCollapse">
+                        <ul className="navbar-nav mr-auto">
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/">Home</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </header>
+        );
+    };
     
-    return (
-        <header>
-            <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-info">
-                <Link className="navbar-brand" to="/">Raise a Question</Link>
-                <div className="collapse navbar-collapse" id="navbarCollapse">
-                    <ul className="navbar-nav mr-auto">
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/">Home</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/class_form">Add Class</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/teacher_view">Teacher View</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/student_view">Student View</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/login">Login</Link>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-        </header>
-    );
+    // return (
+    //     <header>
+    //         <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-info">
+    //             <Link className="navbar-brand" to="/">Raise a Question</Link>
+    //             <div className="collapse navbar-collapse" id="navbarCollapse">
+    //                 <ul className="navbar-nav mr-auto">
+    //                     <li className="nav-item">
+    //                         <Link className="nav-link" to="/">Home</Link>
+    //                     </li>
+    //                     <li className="nav-item">
+    //                         <Link className="nav-link" to="/class_form">Add Class</Link>
+    //                     </li>
+    //                     <li className="nav-item">
+    //                         <Link className="nav-link" to="/teacher_view">Teacher View</Link>
+    //                     </li>
+    //                     <li className="nav-item">
+    //                         <Link className="nav-link" to="/student_view">Student View</Link>
+    //                     </li>
+    //                     <li className="nav-item">
+    //                         <Link className="nav-link" to="/login">Login</Link>
+    //                     </li>
+    //                 </ul>
+    //             </div>
+    //         </nav>
+    //     </header>
+    // );
 };
 
 export default Header;
