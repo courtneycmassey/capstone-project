f99d0ae6d2751619258877be4093f804c9eb2e7d
Courtney <courtneycmassey@gmail.com>
2021-08-17 16:09:38 -0700

    adds ability for teacher to delete a course

--- src/Login.js
+++ src/Login.js
@@ -58,7 +58,7 @@ const Login = ({ history }) => {
                     </div>
                 </div>
             </div>
-            <div class="footer">
+            <div className="footer">
                 <p>© 2021 Courtney Massey</p>
                 <p>image by <a href="https://undraw.co/">undraw.co</a></p>
             </div>
